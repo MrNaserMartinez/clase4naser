833c77ccd1705dc89f2a2411ec9ce072ab835262
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-24 11:23:04 -0600

    p3

--- clase4naser/Program.cs
+++ clase4naser/Program.cs
@@ -21,22 +21,16 @@ Console.WriteLine($"La suma es: {resultado}");
 
 Console.WriteLine("ENUNCIADO #2 /n");
 Console.WriteLine("HASTA ENCONTRAR EL NEGATIVO Y SU SUMA/n");
-
-int suma = 0;
-int numero;
+int suma = 0; int numero;
 do
 {
-    Console.Write("Ingrese un número: ");
-    numero = Convert.ToInt32(Console.ReadLine());
-
+    Console.Write("Ingrese un número: "); numero = Convert.ToInt32(Console.ReadLine());
     if (numero >= 0)
     {
         suma += numero;
     }
 } while (numero >= 0);
-
 Console.WriteLine($"La suma de los números ingresados es: {suma}");
-    }
 
 
 
