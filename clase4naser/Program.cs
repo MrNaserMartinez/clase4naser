a122b67f040de76c8d27c2daaf73fbecb2b5b23b
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-24 11:15:02 -0600

    p2

--- clase4naser/Program.cs
+++ clase4naser/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hola, este es mi programa");
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Hola, este es mi programa");
 
 Console.WriteLine("ENUNCIADO #1 /n");
 Console.WriteLine("primeros numeros pares hasta el 10 /n");
@@ -9,5 +11,35 @@ while (x <= 10)
     x+=2;
 }
 
-Console.WriteLine("primeros numeros pares hasta el 10 /n");
+Console.WriteLine("suma de los primeros numeros naturales /n");
+int resultado = 0; 
+for (int i = 1; i <= 10; i++) 
+{
+    resultado += i;
+}
+Console.WriteLine($"La suma es: {resultado}");
+
+Console.WriteLine("ENUNCIADO #2 /n");
+Console.WriteLine("HASTA ENCONTRAR EL NEGATIVO Y SU SUMA/n");
+
+int suma = 0;
+int numero;
+do
+{
+    Console.Write("Ingrese un número: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero >= 0)
+    {
+        suma += numero;
+    }
+} while (numero >= 0);
+
+Console.WriteLine($"La suma de los números ingresados es: {suma}");
+    }
+
+
+
+
+
 
