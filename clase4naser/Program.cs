a58b8087bab569042cae2e326defb3b9d53e013f
MrNaser <nmartinezm10@miumg.edu.gt>
2024-02-24 10:57:51 -0600

    new p1

--- clase4naser/Program.cs
+++ clase4naser/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.WriteLine("Hola, este es mi programa");
 
+Console.WriteLine("ENUNCIADO #1 /n");
+Console.WriteLine("primeros numeros pares hasta el 10 /n");
+int x = 2;
+while (x <= 10)
+{
+    Console.WriteLine(x);
+    x+=2;
+}
+
+Console.WriteLine("primeros numeros pares hasta el 10 /n");
+
